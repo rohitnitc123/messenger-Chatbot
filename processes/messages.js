2a688d67021caac57d8f5a6456ebd11748935b2f
Rohit Singh <rohitnitc123@gmail.com>
2023-07-28 12:50:04 +0530

    test-commit

--- processes/messages.js
+++ processes/messages.js
@@ -1,91 +1,79 @@
-const request = require('request');
+const request = require("request");
 const { Configuration, OpenAIApi } = require("openai");
-const senderAction = require('../templates/senderAction');
-const sendMessage = require('../templates/sendMessage');
-require('dotenv').config();
-
-//for open ai response
-const axios = require('axios');
-
-//const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions'; // The API endpoint for text completions using the Davinci Codex model
+require("dotenv").config();
+const axios = require("axios");
 
 
-require('dotenv').config();
+const senderAction = require("../templates/senderAction");
+const sendMessage = require("../templates/sendMessage");
 
 
-const processMessage=(event)=>{
-    if (!event.message.is_echo) {
-        const message = event.message;
-        const senderID = event.sender.id;
-        console.log("Received message from senderId: " + senderID);
-        console.log("Message is: " + JSON.stringify(message));
-        if (message.text) {
-            let text = message.text;
-            var request = require("request");
 
+const processMessage = (event) => {
+  if (!event.message.is_echo) {
+    const message = event.message;
+    const senderID = event.sender.id;
+    console.log("Received message from senderId: " + senderID);
+    console.log("Message is: " + JSON.stringify(message));
+    if (message.text) {
+      let text = message.text;
+      var request = require("request");
 
-            let messageToBeSent=getOpenAIReply(text);
-            senderAction(senderID);
-            sendMessage(senderID, {text:messageToBeSent});
-        }
+      let messageToBeSent = getOpenAIReply(text);
+      senderAction(senderID);
+      sendMessage(senderID, { text: messageToBeSent });
     }
-}
+  }
+};
 
 
-const getOpenAIReply=async(message)=>{
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-      });
-      const openai = new OpenAIApi(configuration);
-      try {
-        const completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: message,
-        });
-  
-        return completion.data.choices[0].message.content;
-        
-      } 
-      catch (error) {
-        if (error.response) {
-          console.log(error.response.status);
-          console.log(error.response.data);
-        } else {
-          console.log(error.message);
-        }
-      }
-}
+const getOpenAIReply = async (message) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: message,
+      max_tokens: 64,
+      temperature: 0,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
+    });
+
+    return  response.data.choices[0].text;
+   
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.response
+        ? error.response.data
+        : "There was an issue on the server",
+    });
+  }
+};
 
 
+// const getOpenAIReply = async (message) => {
+//   const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+//   });
+//   const openai = new OpenAIApi(configuration);
+//   try {
+//     const completion = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       messages: message,
+//     });
 
-
-
-
-//interaction with openai
-// async function getOpenAIReply(prompt) {
-//     try {
-//       const response = await axios.post(
-//         OPENAI_API_URL,
-//         {
-//           prompt: prompt,
-//           max_tokens: 1000, // Adjust this value to control the length of the generated reply
-//         },
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-//           },
-//         }
-//       );
-  
-//       return response.data.choices[0].text.trim();
-//     } catch (error) {
-//       console.error('Error fetching OpenAI reply:', error);
-//       throw error;
+//     return completion.data.choices[0].message.content;
+//   } catch (error) {
+//     if (error.response) {
+//       console.log(error.response.status);
+//       console.log(error.response.data);
+//     } else {
+//       console.log(error.message);
 //     }
 //   }
-  
+// };
 
 
-
-module.exports=processMessage;
+module.exports = processMessage;
